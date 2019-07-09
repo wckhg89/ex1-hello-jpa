ae50fa6356e1de96cd570efa5f47023321d8cc96
kanghonggu <guppy.kang@woowahan.com>
2019-07-10 08:21:12 +0900

    Update - JPQL 초간단 실습

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+[
++src/main/resources/META-INF/persistence.xml,3/b/3be4f79b5828b3982f3f6a62bf8fc67b3adedf58
+R
+"src/main/java/hellojpa/Member.java,f/d/fd68fd8c0388b07c61445afa503c8ec018eab4c0
+S
+#src/main/java/hellojpa/JpaMain.java,3/8/3818751ba7a7e2bbfde51b6c3126a02f97f5a644
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,10 +5,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
     public static void main(String[] args) {
+
+
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
@@ -18,12 +22,16 @@ public class JpaMain {
         try {
 //            insert
 //            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("guppyA");
+//            member.setId(2L);
+//            member.setName("guppyB");
 //            em.persist(member);
 
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("guppy.kang");
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("guppy.kang");
+
+            List<Member> members = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+            members.forEach(it -> System.out.println(it.getName()));
 
             tx.commit();
         } catch (Exception e) {
