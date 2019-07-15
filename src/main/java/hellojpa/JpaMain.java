1197e147c80f96509b0347dd331e98a27b64320b
kanghonggu <guppy.kang@woowahan.com>
2019-07-16 08:54:47 +0900

    flush, detatch

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -9,9 +9,6 @@ import javax.persistence.Persistence;
 public class JpaMain {
 
     public static void main(String[] args) {
-
-
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
@@ -19,14 +16,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member memberA = new Member(159L, "A");
-            Member memberB = new Member(160L, "B");
+            // Member member = new Member(1L, "guppy");
+            // em.persist(member);
+            // em.flush(); // 쓰기지연 SQL 저장소 비우기
 
-            em.persist(memberA);
-            em.persist(memberB);
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
+
+            em.detach(member); // 준영속화
 
             System.out.println("=======================");
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
